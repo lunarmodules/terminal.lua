83c617e45f0f777b6268cd1111fbb9722a5260c7
ahmvdev <ahmadsaeed3290@gmail.com>
2025-03-15 00:16:39 +0500

    added tests for foreground base colors

--- spec/05-color_spec.lua
+++ spec/05-color_spec.lua
@@ -1,61 +1,22 @@
 describe("Color Module Tests", function()
+  local color
 
-    local color
-  
-    setup(function()
-      color = require "terminal.color"
-    end)
-  
-  
-  
-    it("should return default scroll region reset sequence", function()
-      assert.are.equal("\27[r", color.scroll_regions())
-    end)
-  
-  
-    it("should return correct scroll region set sequence", function()
-      assert.are.equal("\27[5;10r", scroll.scroll_regions(5, 10))
-    end)
-  
-  
-    it("should return correct sequence for scrolling up by default 1 line", function()
-      assert.are.equal("\27[1S", scroll.scroll_ups())
-    end)
-  
-  
-    it("should return correct sequence for scrolling up by 5 lines", function()
-      assert.are.equal("\27[5S", scroll.scroll_ups(5))
-    end)
-  
-  
-    it("should return correct sequence for scrolling down by default 1 line", function()
-      assert.are.equal("\27[1T", scroll.scroll_downs())
-    end)
-  
-  
-    it("should return correct sequence for scrolling down by 3 lines", function()
-      assert.are.equal("\27[3T", scroll.scroll_downs(3))
-    end)
-  
-  
-    it("should return empty string for zero vertical scroll", function()
-      assert.are.equal("", scroll.scrolls(0))
-    end)
-  
-  
-    it("should return correct sequence for positive vertical scroll (down)", function()
-      assert.are.equal("\27[3T", scroll.scrolls(3))
-    end)
-  
-  
-    it("should return correct sequence for negative vertical scroll (up)", function()
-      assert.are.equal("\27[2S", scroll.scrolls(-2))
-    end)
-  
-  
-    pending("scroll stack tests", function()
-      -- placeholder to do later
-    end)
-  
+  setup(function()
+    color = require "terminal.color".fg_base_colors
   end)
-  
+
+  it("should return correct ANSI code for valid colors", function()
+    assert.are.equal("\27[30m", color.black)
+    assert.are.equal("\27[31m", color.red)
+    assert.are.equal("\27[32m", color.green)
+    assert.are.equal("\27[33m", color.yellow)
+    assert.are.equal("\27[34m", color.blue)
+    assert.are.equal("\27[35m", color.magenta)
+    assert.are.equal("\27[36m", color.cyan)
+    assert.are.equal("\27[37m", color.white)
+  end)
+
+  it("should throw an error for invalid colors", function()
+    assert.has_error(function() return color.invalid end, "invalid string-based color: invalid")
+  end)
+end)
--- src/terminal/color.lua
+++ src/terminal/color.lua
@@ -19,7 +19,7 @@ local blink_off = "\27[25m"
 local reverse_on = "\27[7m"
 local reverse_off = "\27[27m"
 
-local fg_base_colors = setmetatable({
+M.fg_base_colors = setmetatable({
   black = "\27[30m",
   red = "\27[31m",
   green = "\27[32m",
@@ -310,126 +310,6 @@ end
 -- @within textcolor
 function M.brightness(brightness)
   output.write(M.brightnesss(brightness))
-  return true
-end
-
-
---=============================================================================
--- text_stack: colors & attributes
---=============================================================================
--- Text colors and attributes stack.
--- Stack for managing the text color and attributes.
--- @section textcolor_stack
-
-
-local function newtext(attr)
-  local last = _colorstack[#_colorstack]
-  local fg_color = attr.fg or attr.fg_r
-  local bg_color = attr.bg or attr.bg_r
-  local new = {
-    fg         = fg_color        == nil and last.fg         or colorcode(fg_color, attr.fg_g, attr.fg_b, true),
-    bg         = bg_color        == nil and last.bg         or colorcode(bg_color, attr.bg_g, attr.bg_b, false),
-    brightness = attr.brightness == nil and last.brightness or _brightness[attr.brightness],
-    underline  = attr.underline  == nil and last.underline  or (not not attr.underline),
-    blink      = attr.blink      == nil and last.blink      or (not not attr.blink),
-    reverse    = attr.reverse    == nil and last.reverse    or (not not attr.reverse),
-  }
-  new.ansi = attribute_reset .. new.fg .. new.bg ..
-    _brightness_sequence_after_reset[new.brightness] ..
-    (new.underline and underline_on or "") ..
-    (new.blink and blink_on or "") ..
-    (new.reverse and reverse_on or "")
-  -- print("newtext:", (new.ansi:gsub("\27", "\\27")))
-  return new
-end
-
---- Creates an ansi sequence to set the text attributes without writing it to the terminal.
--- Only set what you change. Every element omitted in the `attr` table will be taken from the current top of the stack.
--- @tparam table attr the attributes to set, with keys:
--- @tparam[opt] string|integer attr.fg the foreground color to set. Base color (string), or extended color (number). Takes precedence of `fg_r`, `fg_g`, `fg_b`.
--- @tparam[opt] integer attr.fg_r the red value of the foreground color to set.
--- @tparam[opt] integer attr.fg_g the green value of the foreground color to set.
--- @tparam[opt] integer attr.fg_b the blue value of the foreground color to set.
--- @tparam[opt] string|integer attr.bg the background color to set. Base color (string), or extended color (number). Takes precedence of `bg_r`, `bg_g`, `bg_b`.
--- @tparam[opt] integer attr.bg_r the red value of the background color to set.
--- @tparam[opt] integer attr.bg_g the green value of the background color to set.
--- @tparam[opt] integer attr.bg_b the blue value of the background color to set.
--- @tparam[opt] string|number attr.brightness the brightness level to set
--- @tparam[opt] boolean attr.underline whether to set underline
--- @tparam[opt] boolean attr.blink whether to set blink
--- @tparam[opt] boolean attr.reverse whether to set reverse
--- @treturn string ansi sequence to write to the terminal
--- @within textcolor_stack
-function M.textsets(attr)
-  local new = newtext(attr)
-  return new.ansi
-end
-
---- Sets the text attributes and writes it to the terminal.
--- Every element omitted in the `attr` table will be taken from the current top of the stack.
--- @tparam table attr the attributes to set, see `textsets` for details.
--- @return true
--- @within textcolor_stack
-function M.textset(attr)
-  output.write(newtext(attr).ansi)
-  return true
-end
-
---- Pushes the current attributes onto the stack, and returns an ansi sequence to set the new attributes without writing it to the terminal.
--- Every element omitted in the `attr` table will be taken from the current top of the stack.
--- @tparam table attr the attributes to set, see `textsets` for details.
--- @treturn string ansi sequence to write to the terminal
--- @within textcolor_stack
-function M.textpushs(attr)
-  local new = newtext(attr)
-  _colorstack[#_colorstack + 1] = new
-  return new.ansi
-end
-
---- Pushes the current attributes onto the stack, and writes an ansi sequence to set the new attributes to the terminal.
--- Every element omitted in the `attr` table will be taken from the current top of the stack.
--- @tparam table attr the attributes to set, see `textsets` for details.
--- @return true
--- @within textcolor_stack
-function M.textpush(attr)
-  output.write(M.textpushs(attr))
-  return true
-end
-
---- Pops n attributes off the stack (and returns the last one), without writing it to the terminal.
--- @tparam[opt=1] number n number of attributes to pop
--- @treturn string ansi sequence to write to the terminal
--- @within textcolor_stack
-function M.textpops(n)
-  n = n or 1
-  local newtop = math.max(#_colorstack - n, 1)
-  for i = newtop + 1, #_colorstack do
-    _colorstack[i] = nil
-  end
-  return _colorstack[#_colorstack].ansi
-end
-
---- Pops n attributes off the stack, and writes the last one to the terminal.
--- @tparam[opt=1] number n number of attributes to pop
--- @return true
--- @within textcolor_stack
-function M.textpop(n)
-  output.write(M.textpops(n))
-  return true
-end
-
---- Re-applies the current attributes (returns it, does not write it to the terminal).
--- @treturn string ansi sequence to write to the terminal
--- @within textcolor_stack
-function M.textapplys()
-  return _colorstack[#_colorstack].ansi
-end
-
---- Re-applies the current attributes, and writes it to the terminal.
--- @return true
--- @within textcolor_stack
-function M.textapply()
-  output.write(_colorstack[#_colorstack].ansi)
   return true
 end
 
