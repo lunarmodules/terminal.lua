e0c1180c7cbc83c28489f69aa17e96e118062693
ahmvdev <ahmadsaeed3290@gmail.com>
2025-03-16 04:55:47 +0500

    updated headers

--- src/terminal/stack/color.lua
+++ src/terminal/stack/color.lua
@@ -1,5 +1,5 @@
 -- Module for setting a stack of text colors and attributes
--- Also allows functionality to push,pop color and text attributes.
+-- Also allows functionality to push, pop color and text attributes.
 -- @usage
 -- local colorstack = require("terminal.color.stack")
 
