d2275bf30c415f26161d0fbc5f23f144995f106a
Thijs Schreijer <thijs@thijsschreijer.nl>
2026-02-11 11:39:22 +0100

    test(helpers): added mocking for termsize()

--- spec/helpers.lua
+++ spec/helpers.lua
@@ -8,12 +8,118 @@
 
 local M = {}
 
+local terminal  -- to hold the terminal module if loaded
+local system -- to hold the system module if loaded
 
 
--- Remove terminal and LuaSystem from package.loaded and run GC.
--- Used internally by load() and unload(). Cleans `"terminal"`, `"system"`, and
--- any key starting with `"terminal."`.
-local function clean()
+
+local get_config do
+  -- this config table holds config values as configured for the mock functions.
+  -- the key is the terminal module-table, the value a hash-table of config values.
+  -- Because it is set to weak-key, the table is only kept alive if the terminal module
+  -- is still in use, aftre a reload it will be a fresh copy again.
+  local config = setmetatable({}, { __mode = "k" })
+
+
+  -- returns the mock config for the current modules (system and terminal)
+  -- every mock function should call this to ensure we run the asserts.
+  function get_config()
+    assert(terminal, "modules not loaded yet, first call 'load()'")
+
+    local cfg = config[terminal]
+    if not cfg then
+      cfg = setmetatable({}, {
+        __index = function(self, key)
+          -- the top-level is an "auto-table", if there is no key, we create and return an empty table.
+          -- so we can reduce extensive checking elsewhere in this module
+          self[key] = {}
+          return self[key]
+        end
+      })
+      config[terminal] = cfg
+    end
+    return cfg
+  end
+end
+
+
+
+-- ====================================================================================================
+-- Mock functions for system and terminal
+-- ====================================================================================================
+
+
+
+-- Sets the terminal size.
+-- @tparam number rows number of rows
+-- @tparam number columns number of columns
+function M.set_termsize(rows, columns)
+  assert(type(rows) == "number", "rows must be a number, got " .. type(rows))
+  assert(type(columns) == "number", "columns must be a number, got " .. type(columns))
+
+  get_config().termsize = {
+    rows = rows,
+    columns = columns,
+  }
+end
+
+
+
+--- Gets the terminal size.
+-- This is the mock function for `system.termsize()`.
+-- If the terminal size wasn't set yet, returns 25x80.
+-- @treturn number rows number of rows
+-- @treturn number columns number of columns
+function M.get_termsize()
+  local cfg = get_config()
+  return cfg.termsize.rows or 25, cfg.termsize.columns or 80
+end
+
+
+
+-- ====================================================================================================
+-- (Un)loading and patching system and terminal to enable mocks
+-- ====================================================================================================
+
+
+-- Patches system to enable mocking
+local function patch_system()
+  system.termsize = M.get_termsize
+end
+
+
+
+-- Patches terminal to enable mocking
+local function patch_terminal()
+end
+
+
+
+--- Load the main terminal and system modules after cleaning package.loaded.
+-- Cleans terminal and LuaSystem from `package.loaded`, then requires the main
+-- `terminal` module. Will patch both libraries to enable the mocks.
+-- Call from Busted setup or before_each.
+-- @treturn table the main terminal module
+function M.load()
+  M.unload()
+
+  system = require("system")
+  patch_system()
+
+  terminal = require("terminal")
+  patch_terminal()
+
+  return terminal
+end
+
+
+
+--- Remove terminal and LuaSystem from package.loaded.
+-- Does not load them again. Call from Busted teardown or after_each so the
+-- next load() gets a fresh terminal. Idempotent.
+function M.unload()
+  terminal = nil
+  system = nil
   for key, _ in pairs(package.loaded) do
     if key == "terminal" or
        key == "system" or
@@ -24,26 +130,6 @@ local function clean()
   -- call twice to ensure finalization is complete
   collectgarbage()
   collectgarbage()
-end
-
-
-
---- Load the main terminal module after cleaning package.loaded.
--- Cleans terminal and LuaSystem from `package.loaded`, then requires the main
--- `terminal` module and returns it. Call from Busted setup or before_each.
--- @treturn table the main terminal module
-function M.load()
-  clean()
-  return require("terminal")
-end
-
-
-
---- Remove terminal and LuaSystem from package.loaded.
--- Does not require terminal. Call from Busted teardown or after_each so the
--- next load() gets a fresh terminal. Idempotent.
-function M.unload()
-  clean()
 end
 
 
