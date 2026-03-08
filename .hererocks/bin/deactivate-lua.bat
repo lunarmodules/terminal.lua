0ca758c47ff1f7eb8a0d8dfc16e147b370b37448
Vinit-786 <vinitmahajan175@gmail.com>
2026-03-08 11:51:48 +0530

    chore: remove accidentally committed hererocks files

--- .hererocks/bin/deactivate-lua.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-if exist "C:\Users\vinit\Documents\Projects_VSCode\CPP\dsa\web dev\terminal.lua\.hererocks\bin\lua.exe" for /f "usebackq delims=" %%p in (`""C:\Users\vinit\Documents\Projects_VSCode\CPP\dsa\web dev\terminal.lua\.hererocks\bin\lua" "C:\Users\vinit\Documents\Projects_VSCode\CPP\dsa\web dev\terminal.lua\.hererocks\bin\get_deactivated_path.lua""`) DO set "PATH=%%p"
