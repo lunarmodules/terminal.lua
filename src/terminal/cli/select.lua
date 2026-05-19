e2962dd0222ecbd4b1a870758e302863f95fd037
Thijs Schreijer <thijs@thijsschreijer.nl>
2026-05-19 20:56:12 +0200

    fix(select): remove vim mapping, collide with typing

--- src/terminal/cli/select.lua
+++ src/terminal/cli/select.lua
@@ -40,9 +40,7 @@ local Select = utils.class()
 local keys = t.input.keymap.get_keys()
 local ctrl_c = assert(t.input.keymap.get_raw_key("ctrl_c"))
 local keymap = t.input.keymap.get_keymap({
-  k = keys.up,    -- Vim-style up
-  j = keys.down,  -- Vim-style down
-  [ctrl_c] = keys.escape, -- Ctrl+C --> Esc
+  [ctrl_c] = keys.escape, -- remap Ctrl+C --> Esc
 })
 
 
